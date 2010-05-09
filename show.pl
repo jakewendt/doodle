3f8ef155ecaf4e3c16eb972d2a8f7ea0694031e0
Jake <github@jake.otherinbox.com>
2010-05-09 14:04:33 -0700

    A little code cleanup

--- show.pl
+++ show.pl
@@ -3,12 +3,12 @@
 use strict;
 use CGI qw(:standard);
 
-my $pic  = param ( "pic" );
+my $pic  = param ( "pic" ) || 'missing';
 
 print header;
 print start_html(
 		-title => "Your new pic",
-		-head  =>  Link({-rel=>'icon', -href=>"tmp/$pic.png", -type=>'image/icon' }),
+		-head  => Link({-rel=>'icon', -href=>"tmp/$pic.png", -type=>'image/icon' })
 	);
 print "<center>";
 print img { src => "tmp/$pic.png" };
